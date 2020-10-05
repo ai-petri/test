0ad8fdaed1928415c5cf2e75c852926deef9a139
Dmitry <234518@mail.ru>
2020-10-06 01:34:35 +0300

    скролл страницы при клике на стрелочку

--- src/Menu.js
+++ src/Menu.js
@@ -3,14 +3,6 @@ import React from "react";
 function Menu(props)
 {
 
-    function goTo(id)
-    {
-        var element = document.getElementById(id);
-        if(element)
-        {
-            element.scrollIntoView({ behavior: 'smooth' });
-        }
-    }
 
     var links = ["home", "about me", "portfolio", "contact"];
 
@@ -19,9 +11,9 @@ function Menu(props)
     return <>  
         <nav className="menu">
             {links.map((link,index)=>
-            <span key={index} style={style} onClick={()=>goTo(index + 1)}>
+            <a key={index} style={style} href={"#"+(index+1)}>
                 {link.toUpperCase()} 
-            </span>)}
+            </a>)}
     </nav>
     <svg className="centered line" style={style}>
     <rect width="100%" height="1"/>
