bc1116f1f45f7cdd243fb3bd9c6515e96abf16d0
Dmitry <234518@mail.ru>
2020-10-08 21:52:42 +0300

    server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 var app = express();
-
+var port = process.env.PORT || 80;
 app.use(express.static('./dist'));
-app.listen(80);
+app.listen(port);
 
 process.stdin.setRawMode(true);
 process.stdin.on("data", data=>{process.exit()});
 
-console.log("\n \x1b[94m server started \x1b[0m");
+console.log("\n \x1b[94m server started \x1b[0m \n\n *****************************************\n\n\t http://localhost:"+port);
+console.log("\n *****************************************\n");
